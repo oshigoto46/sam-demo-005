f9210e8cdfafd66c4c974843172c3551a173949a
Hirohito Tsuru <hirohito.tsuru@tri-ad.global>
2020-08-16 23:03:27 +0900

    init

---
+++ hello_world/app.py
@@ -0,0 +1,13 @@
+import logging
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+def lambda_handler(event, context):
+    logger.info(event)
+    return {
+        "statusCode": 200,
+        "body": {
+            "message": "hello world",
+        }
+    }
