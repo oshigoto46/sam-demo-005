5f19aef72812fe2a8219a0bd16cc771ae21628a1
shiro.tabuchi@doublero.com <shirotabuchi@doublero.com>
2021-11-14 14:25:55 +0200

    lambda function change

--- hello_world/app.py
+++ hello_world/app.py
@@ -1,13 +1,17 @@
+import json
 import logging
+import os
+import time
 
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
 def lambda_handler(event, context):
-    logger.info(event)
-    return {
-        "statusCode": 200,
-        "body": {
-            "message": "hello world",
-        }
-    }
+    logger.info(json.dumps(event))
+    sleeptime= int(os.environ.get("SLEEP","2")) 
+    time.sleep(sleeptime)
+    if (os.environ.get("BEHAVIOR") == "NG") :
+        logger.error('failed function')
+        raise Exception('failed!')
+    else :
+        return (event)
